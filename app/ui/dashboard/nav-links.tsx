43ea5036ddf31bd4605b772042be2f1f10586b03
NAJEEHAT APEKE WAHAB <wahabnanjeehat@gmail.com>
2024-08-19 08:35:20 +0000

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,12 +1,11 @@
 "use client";
-
+import { usePathname } from 'next/navigation';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
